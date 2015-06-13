6bf34ae0c7cb94319487a89edee90f0acd257226
Juan C. Martinez <martinez@sancristobalsl.com>
2015-06-13 13:36:53 +0200

    créditos

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+// COMPLETAR PARA QUE SIRVA LA PÁG views/author.ejs  
+router.get('/author', function (req,res) {
+	res.render('author', { autor: 'Juan C. Martinez' });
+});
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
